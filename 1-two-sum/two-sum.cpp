60ca11d74f1d5ad195e76f618b394103e74a80d2
Eugene <eugenexu@umich.edu>
2025-01-06 11:35:04 -0500

    Time: 0 ms (100.00%) | Memory: 14.8 MB (26.83%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
+        for (int i=0; i<nums.size(); i++){
+            int complement = target - nums[i];
+            if (hashMap.find(complement) != hashMap.end()){
+                return {i,hashMap[complement]};
+            }
+            hashMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
