167d61aaeddd62fa42e9e9532da8ff99abf33edc
Eugene <eugenexu@umich.edu>
2026-05-03 21:16:14 -0400

    Time: 3 ms (50.45%) | Memory: 27.8 MB (79.56%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,23 +8,27 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #Thank you amy shi for your kind pateince
-        #with silly old me
-        # i shudve believed u...
-        arr = []
-        ptr = head
-        while ptr:
-            arr.append(ptr)
-            ptr = ptr.next
-
-        i, j = 0, len(arr) - 1
-        while i < j:
-            if i != 0:
-                arr[j+1].next = arr[i]
-            arr[i].next = arr[j]
-            i += 1
-            j -= 1
-        if i == j and len(arr) % 2 == 1 and len(arr) > 1:
-            arr[i+1].next = arr[i]
-        arr[i].next = None
-        return head
+        #find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        #merge first and second half
+        one, two = head, prev
+        while one and two:
+            temp1 = one.next
+            temp2 = two.next
+            one.next = two
+            two.next = temp1
+            one = temp1
+            two = temp2
