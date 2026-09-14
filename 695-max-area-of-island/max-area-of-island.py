ad52c229028b3eb6f4a6544a2bdcca1ae15e7491
Eugene <eugenexu@umich.edu>
2026-09-13 20:11:56 -0400

    Time: 23 ms (40.71%) | Memory: 19.4 MB (89.44%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,24 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
         def bfs(i, j):
             #print(f'searching {i=}, {j=}')
             queue = deque([[i, j]])
             area = 1
             while queue:
                 i, j = queue.popleft()
-                node = grid[i][j]
-                surrounding = [[i - 1, j], [i, j - 1], [i + 1, j], [i, j + 1]]
+                surrounding = ((i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1))
                 for ni, nj in surrounding:
-                    if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and not visited[ni][nj] and grid[ni][nj] == 1:
+                    if 0 <= ni < rows and 0 <= nj < cols and not visited[ni][nj] and grid[ni][nj] == 1:
                         visited[ni][nj] = True
                         queue.append([ni, nj])
                         area += 1
             #print(f'{area=}')
             return area
         ans = 0
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
                 node = grid[i][j]
                 if node == 1 and not visited[i][j]:
                     visited[i][j] = True
