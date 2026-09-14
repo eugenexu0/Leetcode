f5c1a66539f2826144089ad9d8eb332de3318b73
Eugene <eugenexu@umich.edu>
2026-09-14 15:03:01 -0400

    Time: 256 ms (32.19%) | Memory: 21.5 MB (92.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,19 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
         def bfs(i, j):
             stack = [(i,j)]
             while stack:
                 x, y = stack.pop()
                 surrounding = ((x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1))
                 for nx, ny in surrounding:
-                    if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and not visited[nx][ny] and grid[nx][ny] == '1':
+                    if 0 <= nx < rows and 0 <= ny < cols and not visited[nx][ny] and grid[nx][ny] == '1':
                         stack.append((nx, ny))
                 visited[x][y] = True
 
         ans = 0
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == '1' and not visited[i][j]:
                     bfs(i, j)
                     ans += 1
