c6af610574b059756561f3f160d75dae3d029208
Eugene <eugenexu@umich.edu>
2026-09-14 14:38:41 -0400

    Time: 29 ms (97.62%) | Memory: 24.6 MB (41.56%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -9,17 +9,8 @@ class Solution:
         #print(f'{self.prefixsum=}')
 
     def pickIndex(self) -> int:
-        target = random.randint(1, self.prefixsum[-1])
-        left, right = 0, len(self.prefixsum) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if self.prefixsum[mid] == target:
-                return mid
-            elif self.prefixsum[mid] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
+        target = random.random() * self.prefixsum[-1]
+        return bisect.bisect_left(self.prefixsum, target)
         
 # [2, 5, 3, 1, 4]
 # sum = 15
