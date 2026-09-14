8d446e446eb69c89dfab9019eaee26aacddb2ec5
Eugene <eugenexu@umich.edu>
2026-09-14 14:37:29 -0400

    Time: 91 ms (25.88%) | Memory: 24.7 MB (22.77%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,17 +1,31 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.nums = w
-        self.total = sum(w)
+        self.prefixsum = []
+        tempsum = 0
+        for n in w:
+            tempsum += n
+            self.prefixsum.append(tempsum)
+        #print(f'{self.prefixsum=}')
 
     def pickIndex(self) -> int:
-        rand = random.randint(1, self.total)
-        tempsum = 0
-        for i, n in enumerate(self.nums):
-            tempsum += n
-            if tempsum >= rand:
-                return i
-
+        target = random.randint(1, self.prefixsum[-1])
+        left, right = 0, len(self.prefixsum) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if self.prefixsum[mid] == target:
+                return mid
+            elif self.prefixsum[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+        
+# [2, 5, 3, 1, 4]
+# sum = 15
+# [2, 7, 10, 11, 15]
+# 1 - 15
+# 4
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
