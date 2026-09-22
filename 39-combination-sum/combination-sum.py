7cb540d03eb1be5d9fe4e11af532ef1ccbbb3ab5
Eugene <eugenexu@umich.edu>
2026-09-22 19:00:02 -0400

    Time: 11 ms (45.96%) | Memory: 19.8 MB (10.33%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,18 +1,15 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def backtrack(res: List[int], path: List[int], remain: int, index: int):
-            #check if remaining reached 0
-            if remain == 0:
-                res.append(path.copy())
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        ans = []
+        def backtrack(path, total, index):
+            if total == target:
+                ans.append(path[:])
+            if total > target:
                 return
-            #prune if gets below 0
-            elif remain < 0:
-                return
-            #note that index is needed to prevent duplicate paths
             for i in range(index, len(candidates)):
-                newpath = path.copy()
-                newpath.append(candidates[i])
-                backtrack(res, newpath, remain - candidates[i], i)
-        res, path = [], []
-        backtrack(res, path, target, 0)
-        return res
+                path.append(candidates[i])
+                backtrack(path, total + candidates[i], i)
+                path.pop()
+        backtrack([], 0, 0)
+        return ans
+        
